40bb0890cc14009626acf4cc043537db23ab2ce8
Peter BUSS <peter.buss@hispeed.ch>
2016-04-14 22:45:42 +0000

    Create Article successfully

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -7,8 +7,8 @@ RSpec.feature "Creating Articles" do
     click_link "New Article"
     
     fill_in "Title", with: "Creating first article"
-    file_in "Body", with: "Lorem Ipsum"
-    click_button "Create Aritcle"
+    fill_in "Body", with: "Lorem Ipsum"
+    click_button "Create Article"
     
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
